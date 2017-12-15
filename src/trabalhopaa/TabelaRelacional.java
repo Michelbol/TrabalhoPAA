67e15922942bc82d37a9fcaa9f3e1e0b4454b7eb
Michel <michel.bolzon123@gmail.com>
2017-12-14 22:56:12 -0200

    Implemetei o que ta implementado

---
+++ src/trabalhopaa/TabelaRelacional.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhopaa;
+
+/**
+ *
+ * @author miche
+ */
+public class TabelaRelacional {
+    private int valor;
+    private Posicao posicao;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public Posicao getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(Posicao posicao) {
+        this.posicao = posicao;
+    }
+    
+        public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+}
--- src/trabalhopaa/TrabalhoPAA.java
+++ src/trabalhopaa/TrabalhoPAA.java
@@ -5,6 +5,9 @@
  */
 package trabalhopaa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author miche
@@ -16,8 +19,41 @@ public class TrabalhoPAA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int x[] = null;
+        int y[] = null;
         
-        System.out.println("Heeloow Trabalho Paa");
+        System.out.println("Veitor: " + Lcs_Length(x,y));
+    }
+    
+    public static ArrayList Lcs_Length(int x[], int y[]){
+        ArrayList retorno = new ArrayList();
+        int m = x.length;
+        int n = y.length;
+        TabelaRelacional c[][] = new TabelaRelacional[m][n];
+        TabelaRelacional b[][] = new TabelaRelacional[m][n];
+        for(int i = 1; i < m; i++){
+            c[i][0].setValor(12);
+        }
+        for(int j = 1; j < n; j++){
+            c[0][j].setValor(0);;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(x[i] == y[j]){
+                    c[i][j].setValor((c[i-1][j-1].getValor()+1));
+                    b[i][j].setPosicao(Posicao.Diagonal);
+                }else if(c[i-1][j].getValor() >= c[i-1][j].getValor()){
+                    c[i][j].setValor(c[i-1][j].getValor());
+                    b[i][j].setPosicao(Posicao.Cima);
+                }else{
+                    c[i][j].setValor(c[i][j-1].getValor());
+                    b[i][j].setPosicao(Posicao.Esquerda);
+                }
+            }
+        }
+        retorno.add(c);
+        retorno.add(b);
+        return retorno;
     }
     
 }
